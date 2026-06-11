3645a37e60e8f3cbb7b50e0141ae2c92bbe52cb8
Nick O'Connor <nioc4468@colorado.edu>
2026-06-10 19:28:57 -0500

    changed psycopg2 requirement to psycopg v3

--- db_test.py
+++ db_test.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-import psycopg2
+import psycopg
 from datetime import datetime
 
 load_dotenv()
@@ -8,22 +8,22 @@ load_dotenv()
 db_username = os.getenv("DB_USERNAME")
 db_password = os.getenv("DB_PASSWORD")
 
-connection = psycopg2.connect(database="py-proj", user=db_username, password=db_password, host="localhost", port=5432)
-
-cursor = connection.cursor()
-
-cursor.execute("SELECT * FROM video_info;")
-record = cursor.fetchall()
-print("Data from Databse:- ", record)
-
-values = ('Python Video', datetime.now(), 7)
-cursor.execute(
-    "INSERT INTO video_info (video_name, upload_date, like_count) \
-        VALUES (%s, %s, %s)",
-    values
-    )
-connection.commit()
-
-cursor.execute("SELECT * FROM video_info;")
-record = cursor.fetchall()
-print("Data from Databse:- ", record)
+with psycopg.connect(f"dbname=py-proj user={db_username} password={db_password} host=localhost port=5432") as connection:
+    with connection.cursor() as cursor:
+        
+        cursor.execute("SELECT * FROM video_info;")
+        records = cursor.fetchall()
+        print("Data from Database:- ", records)
+        
+        values = ('New Version Video', datetime.now(), 1738)
+        cursor.execute(
+            "INSERT INTO video_info (video_name, upload_date, like_count) \
+                VALUES (%s, %s, %s)",
+            values
+        )
+        connection.commit()
+        print("Pushed video")
+        
+        cursor.execute("SELECT * FROM video_info;")
+        records = cursor.fetchall()
+        print("Data from Database:- ", records)
