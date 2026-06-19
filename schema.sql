e7687d975fb8b423aca57fc2dca2901120ab0508
nick0connor <nioc4468@colorado.edu>
2026-06-18 23:55:42 -0500

    Reimplemented FK on channel_id

--- schema.sql
+++ schema.sql
@@ -1,6 +1,9 @@
--- TEXT is the same thing as VARCHAR with no true limit in PostgreSQL
--- TIMESTAMPTZ accounts for timezone
--- _loaded_at is a pipeline audit column: when each row entered system, separate from snapshot_at
+-- NOTES: 
+-- _loaded_at is a pipeline audit column (use snapshot_at if timestamp needed)
+-- 
+-- stg_videos.channel_id will only be NULL IFF video is sourced from the historical CSV data
+-- Modern API calls include the channel_id and thus stg_videos.channel_id IS NOT NULL = true
+-- I use this split to denote modern/historical in the database. Sloppy, but efficient
 
 CREATE TABLE IF NOT EXISTS stg_categories (
     category_id     TEXT PRIMARY KEY,
@@ -15,7 +18,7 @@ CREATE TABLE IF NOT EXISTS stg_channels (
 
 CREATE TABLE IF NOT EXISTS stg_videos (
     video_id        TEXT PRIMARY KEY,
-    channel_id      TEXT, -- potentially NULL due to not all datasets having channel information
+    channel_id      TEXT REFERENCES stg_channels(channel_id), -- NULL for CSV-sourced videos
     category_id     TEXT NOT NULL REFERENCES stg_categories(category_id),
     title           TEXT NOT NULL,
     published_at    TIMESTAMPTZ,
