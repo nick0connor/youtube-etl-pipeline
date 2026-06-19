7e29f14c97b0bd22859fbc0f730a992791d9f59f
nick0connor <nioc4468@colorado.edu>
2026-06-18 22:31:54 -0500

    added gemini client

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@
 
 import os
 import psycopg
+from google import genai
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -18,8 +19,23 @@ def get_connection() -> psycopg.Connection:
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT")
     )
+
+def get_gemini_client() -> genai.Client:
+    """
+    Returns:
+        genai.Client: Configured Gemini client using the API key from .env
+    """
+    api_key = os.getenv("GEMINI_API_KEY")
+    return genai.Client(api_key=api_key)
     
 if __name__ == "__main__":  # pragma: no cover
     conn = get_connection()
     print("Connection successful:", conn)
     conn.close()
+    print()
+    client = get_gemini_client()
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Explain how AI works in a few words",
+    )
+    print(response.text)
